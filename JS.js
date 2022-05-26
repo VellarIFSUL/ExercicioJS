c8a0c2c903b058f80d43d3f33b0f883346b1fced
VellarIFSUL <105016718+VellarIFSUL@users.noreply.github.com>
2022-05-26 20:29:38 -0300

    segunda versão

--- JS.js
+++ JS.js
@@ -3,19 +3,23 @@ var exe01 = document.getElementById("exe01");
 function IN01(){
     exe01.children[0].src="img/bates OFF.png";
 };
-function OUT02(){
+function OUT01(){
     exe01.children[0].src="img/bates ON.png";
 }
 
 //exe 02
 var exe02 = document.getElementById("exe02");
+var exe02Int = exe02.children[1];
+var lamp = false;
 function ON02(){
     exe02.children[0].src="img/lampada_on.jpg";
-    exe02.children[1].src="img/interruptor_on.jpg";
+    exe02Int.src="img/interruptor_on.jpg";
+    lamp = true;
 }
 function OFF02(){
     exe02.children[0].src="img/lampada_off.jpg";
-    exe02.children[1].src="img/interruptor_off.jpg";
+    exe02Int.src="img/interruptor_off.jpg";
+    lamp = false;
 }
 
 //exe03
@@ -28,4 +32,25 @@ function torneiraOFF(){
 }
 
 //exe04
+function ConfCPF(){
+    function campoCPF() {
+        if (event.keyCode >= 48 && event.keyCode <= 57) {
+            return true;
+        } else {
+            event.preventDefault();
+            return false;
+        }
+    }
+}
 
+window.onload = function (){
+    //exe 01 listener
+    exe01.addEventListener("mouseenter", IN01,false);
+    exe01.addEventListener("mouseleave", OUT01, false);
+
+    //exe 02
+    exe02Int.addEventListener("click", lamp? ON02:OFF02, false);
+    //exe 04
+    var cpf = document.getElementById("cpf");
+    cpf.addEventListener("keypress", ConfCPF, false);
+}
